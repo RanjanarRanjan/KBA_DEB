1c648893a4332b5ba3dd71f5e414c130c3aaf939
RanjanarRanjan <ranjanranjana509@gmail.com>
2025-01-05 22:48:21 +0530

    update

---
+++ JS/loop/object.js
@@ -0,0 +1,10 @@
+
+function genrate(event)
+        {
+            name=document.getElementById("name").value;
+            C=document.getElementById("class").value
+            document.getElementById("outputdiv").style.display = "block";
+            document.getElementById("iname").innerHTML="St.Joseph's High School"
+            document.getElementById("output").innerHTML="Name : "+name
+            document.getElementById("output1").innerHTML="Class:" +C
+        }
